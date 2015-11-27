eb03bcdf06c3d19cce783ee81d4ba56b3dc838de
catherineomary <catherine.omary@gmail.com>
2015-11-27 17:06:09 -0600

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,27 @@
+
+#Main, takes input from the sensor and the keypad and turns the fan and the humidifier 
+#on or off based on the return values of the sensor and the keypad 
+
+#coded by Catherine O'Mary
+#import sensor and keypad libraries
+
+#*****************************Gathering data*********************************#
+
+#Get min temp from keypad
+print "Enter the temperature at which you would like the fan to turn off:"
+min_temp=
+#Get max temp fro keyboard
+print "Enter the temperature at which you would like the fan to turn on:"
+max_temp=
+
+#Get min hum from keypad
+print "Enter the humidity at which you would like the humidifier to turn on:"
+min_hum=
+
+#Get max hum from keypad
+print "Enter the humidity at which you would like the humidifier to turn off:"
+max_hum=
+
+#*****************************Making Decisions*******************************#
+
+#Breaks down the input from the sensor into humidity and temperature
