002de4cc71c5c5f8f4782501343e4ff493babaa3
Reena Suh <reena.suh3259@gmail.com>
2015-12-02 21:52:28 -0600

    comments and changes
    commented the comments with #
    and also change the pins to 21 & 22

--- Driver.py
+++ Driver.py
@@ -16,7 +16,7 @@ def main:
     #coded by Catherine O'Mary
     #import sensor and keypad libraries
 
-    “””*****************************Gathering data*********************************”””
+    #“””*****************************Gathering data*********************************”””
 
     #Get min temp from keypad
     min_temp=raw_input("Enter the temperature at which you would like the fan to turn off: ")
@@ -35,7 +35,7 @@ def main:
        max_hum=raw_input("Make sure the max humidity is greater than the minimum humidity: ")
 
 
-    “””*****************************Making Decisions*******************************”””
+    #“””*****************************Making Decisions*******************************”””
 
     quit=False
     #loops forever 
@@ -43,14 +43,14 @@ def main:
         #Break down the input from the sensor into humidity and temperature
         humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
-        “””try to get the temperature in the range (min_temp,max_temp) non-inclusive by controlling the fan”””
+        #“””try to get the temperature in the range (min_temp,max_temp) non-inclusive by controlling the fan”””
         if temperature < min_temp:
             #turn fan off, it’s too cold
 	          off(1)
         elif temperature >max_temp:
             #turn fan on, it’s too hot
             on(1)
-        “””try to get the humidity in the range (min_hum,max_hum) non-inclusive by controlling the humidifier”””
+        #“””try to get the humidity in the range (min_hum,max_hum) non-inclusive by controlling the humidifier”””
         if temperature < min_hum:
             #turn humidifier on, it’s too dry
             on(2)
@@ -62,7 +62,7 @@ def main:
 #This function is from github
 def on(outlet):
     #! /usr/bin
-    relay_pins = {'one': 11, 'two':7, 'three':12, 'four':16, 'five':18, 'six':22, 'seven':15, 'eight':13}
+    relay_pins = {'one': 21, 'two':22} #'three':12, 'four':16, 'five':18, 'six':22, 'seven':15, 'eight':13}
     GPIO.setmode(GPIO.BOARD) # use P1 header pin numbering convention
     GPIO.setwarnings(False) # don't want to hear about how pins are already in use
     
@@ -73,7 +73,7 @@ def on(outlet):
 def off(outlet)
     #! /usr/bin/python
 
-    relay_pins = {'one': 11, 'two':7, 'three':12, 'four':16, 'five':18, 'six':22, 'seven':15, 'eight':13}
+    relay_pins = {'one': 21, 'two':22} #'three':12, 'four':16, 'five':18, 'six':22, 'seven':15, 'eight':13}
     GPIO.setmode(GPIO.BOARD) # use P1 header pin numbering convention
     GPIO.setwarnings(False) # don't want to hear about how pins are already in use
     
